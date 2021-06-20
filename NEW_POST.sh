c0470f446e32d586978a1c93e6fcfd87654ec934
www42 <thomas.jaekel@gfn.training>
2021-06-20 21:40:39 +0200

    Add new draft GitHub Actions

--- NEW_POST.sh
+++ NEW_POST.sh
@@ -1,6 +1,8 @@
 # Be sure you are in the right directory.
 # List previos posts
+tree -FL 1
 ls -l _posts
+ls -l _drafts
 
 # Now create
 #    a new postfile (markdown)
@@ -9,28 +11,34 @@ ls -l _posts
 today=$(date +"%Y-%m-%d")
 fulldate=$(date +"%Y-%m-%d %T %z")
 
-title='"Jupyter Notebooks in VS Code"'
-title2="$today-jupyter-notebooks-in-vs-code"
+title='"GitHub Actions - Getting started"'
+title2="$today-github-actions-getting-started"
 
-categories="VS-Code Jupyter"
+categories="GitHub Azure Bicep"
 
 imagedir="assets/images/$title2"
 mkdir $imagedir
 touch $imagedir/.git_keep
 
-postfile="_posts/$title2.md"
-echo "---"                           > $postfile
-echo "layout: post"                 >> $postfile
-echo "title: $title"                >> $postfile
-echo "date: $fulldate"              >> $postfile
-echo "categories: $categories"      >> $postfile
-echo "image1: /$imagedir/dummy.png" >> $postfile
-echo "---"                          >> $postfile
-echo ""                             >> $postfile
-echo "## How it works"              >> $postfile
-echo ""                             >> $postfile
+postfile="_drafts/$title2.md"
+# postfile="_posts/$title2.md"
+
+echo "---"                                   > $postfile
+echo "layout: post"                         >> $postfile
+echo "title: $title"                        >> $postfile
+echo "date: $fulldate"                      >> $postfile
+echo "categories: $categories"              >> $postfile
+echo "image1: /$imagedir/dummy.png"         >> $postfile
+echo "---"                                  >> $postfile
+echo ""                                     >> $postfile
+echo "[Microsoft Docs]:https://docs.microsoft.com/en-us/" >> $postfile
+echo ""                                     >> $postfile
+echo "## How it works"                      >> $postfile
+echo ""                                     >> $postfile
+echo "[See documentation.][Microsoft Docs]" >> $postfile
+echo ""                                     >> $postfile
 echo '<img src="{{ page.image1 | relative_url }}" alt="dummy" width="900"/>' >> $postfile
-echo ""                             >> $postfile
+echo ""                                     >> $postfile
 
 
 cat $postfile
@@ -38,4 +46,5 @@ cat $postfile
 # Next steps:
 #    * Edit the postfile (Markdown)
 #    * Save images to imagedir
-#    * Test it locally: bundle exec jekyll serve [--drafts]
+#    * Test it locally: bundle exec jekyll serve 
+#                       bundle exec jekyll serve --drafts
---
+++ _drafts/2021-06-20-github-actions-getting-started.md
@@ -0,0 +1,16 @@
+---
+layout: post
+title: "GitHub Actions - Getting started"
+date: 2021-06-20 12:40:54 +0200
+categories: GitHub Azure Bicep
+image1: /assets/images/2021-06-20-github-actions-getting-started/dummy.png
+---
+
+[Microsoft Docs]:https://docs.microsoft.com/en-us/
+
+## How it works
+
+[See documentation.][Microsoft Docs]
+
+<img src="{{ page.image1 | relative_url }}" alt="dummy" width="900"/>
+
